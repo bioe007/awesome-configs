447f2d793b32ff75fc7fc3a76ee17ca61cdb3f46
perry <perry.hargrave@gmail.com>
2009-07-24 08:26:48 -0700

    trying to improve play interface

--- mocp.lua
+++ mocp.lua
@@ -1,24 +1,39 @@
-local io = io
-local string = string
-local awful = require("awful")
+local io        = io
+local string    = string
+local awful     = require("awful")
 local beautiful = require("beautiful")
-local naughty = require("naughty")
-local markup = require("markup")
+local naughty   = require("naughty")
+local markup    = require("markup")
+local button    = button
+
+local print = print -- debug
 
 module("mocp")
 
--- public settings
-settings = {}
-settings.iScroller = 1
-settings.MAXCH = 15
-settings.interval = 0.75
+--{{{ variables
+local COMMANDS = {
+  ["PLAY"]  = 'mocp --play',
+  ["PAUSE"] = 'mocp --toggle-pause',
+  ["FWD"]   = 'mocp --next',
+  ["REV"]   = 'mocp --previous' ,
+  ["STOP"]  = 'mocp --stop'
+}
 
 local mocbox = nil
-local trackinfo = {}
-trackinfo.artist = ""
-trackinfo.songtitle = ""
-trackinfo.album = ""
-trackinfo.state = ""
+local trackinfo = {
+  artist    = "",
+  songtitle = "",
+  album     = "",
+  state     = ""
+}
+
+-- public settings
+settings = {
+  iScroller = 1,
+  MAXCH     = 15,
+  interval  = 0.75,
+}
+--}}}
 
 ---{{{ local state()
 -- updates trackinfo.state [ PLAY|PAUSE|STOP|OFF ]
@@ -59,18 +74,22 @@ local function setTitle()
 
   local fd = {}
 
-  if state() then
+  if not (trackinfo.state == "OFF" or trackinfo.state == "STOP") then --state() then
     fd = io.popen('mocp -i')
 
     -- read to end of mocp -i
     for line in fd:lines() do
       key = string.match(line,"^%w+")
       if trackinfo[key:lower()] ~= nil then
-        trackinfo[key:lower()] = awful.util.escape(string.gsub(string.gsub(line,key..":%s*",""),"%b()",""))
+        trackinfo[key:lower()] = awful.util.escape(
+                    string.gsub(string.gsub(line,key..":%s*",""),"%b()",""))
       end
     end
+    fd:close()
+  else
+
+    print("not state")
   end
-  fd:close()
 
 end
 ---}}}
@@ -124,76 +143,9 @@ local function getTime()
 end
 ---}}}
 
----{{{ popup
--- displays a naughty notificaiton of the current track
-function popup()
-  -- awful.hooks.timer.unregister(popup)
-  setTitle()
-  notdestroy()
-
-  local np = {}
-  np.state = nil
-  np.strng = ""
-  if not state() then
-    return
-  else
-    np.strng =  "Artist: "..markup.fg(beautiful.fg_normal,trackinfo.artist).."\n"..
-                "Song:   "..markup.fg(beautiful.fg_normal,trackinfo.songtitle).."\n"..
-                "Album:  "..markup.fg(beautiful.fg_normal,trackinfo.album).."\n"
-    np.strng = np.strng..markup.fg(beautiful.fg_normal,getTime())
-  end
-  np.strng = markup.fg( beautiful.fg_focus, markup.font("monospace", np.strng.."  "))  
-  mocbox = naughty.notify({ 
-    title = markup.font("monospace","Now Playing:"),
-    text = np.strng, hover_timeout = ( settings.hovertime or 3 ), timeout = 3,
-    -- icon = "/usr/share/icons/gnome/24x24/actions/edia-playback-start.png", icon_size = 24,
-    run = function() play(); popup() end
-  })
-end
----}}}
-
----{{{ mocplay() 
--- easier way to check|run mocp
-function play() 
-  if trackinfo.state == "OFF" then
-    awful.util.spawn('mocp --server')
-    state()
-    play()
-  elseif trackinfo.state == "STOP" then
-    awful.util.spawn('mocp --play',false) 
-  elseif trackinfo.state == "PLAY" then
-    awful.util.spawn('mocp --next',false)
-  else 
-    if trackinfo.state == "PLAY" then
-      trackinfo.state = "PAUSE"
-    else
-      trackinfo.state = "PLAY"
-    end
-
-    awful.util.spawn('mocp --toggle-pause',false)
-  end
-end
----}}}
-
-function setwidget(w)
-  settings.widget = w
-  awful.hooks.timer.register (settings.interval,scroller)
-  state()
-end
-
----{{{ function update ( k, v)
--- called by any kind of external script to trigger widget text update
-function update ( k, v )
-  if #k == 0 or #v == 0 then return end
-  if trackinfo[k] ~= nil then
-    trackinfo[k] = v
-  end
-  state()
-end
----}}}
-
--- {{{ mocp widget, scrolls text
-function scroller(tb)
+-- {{{ local scroller 
+-- mocp widget, scrolls text
+local function scroller(tb)
   local np = {}
 
   -- if mocp is not running, then simply return here
@@ -237,6 +189,98 @@ function scroller(tb)
   end
 end
 -- }}}
+
+---{{{ local popup
+-- displays a naughty notificaiton of the current track
+local function popup()
+  -- awful.hooks.timer.unregister(popup)
+  setTitle()
+  notdestroy()
+
+  local np = {}
+  np.state = nil
+  np.strng = ""
+  if not state() then
+    return
+  else
+    np.strng = "Artist: "..markup.fg(beautiful.fg_normal,trackinfo.artist).."\n"..
+               "Song:   "..markup.fg(beautiful.fg_normal,trackinfo.songtitle).."\n"..
+               "Album:  "..markup.fg(beautiful.fg_normal,trackinfo.album).."\n"
+    np.strng = np.strng..markup.fg(beautiful.fg_normal,getTime())
+  end
+  np.strng = markup.fg( beautiful.fg_focus, markup.font("monospace", np.strng.."  "))  
+  mocbox = naughty.notify({ 
+    title = markup.font("monospace","Now Playing:"),
+    text = np.strng, hover_timeout = ( settings.hovertime or 3 ), timeout = 3,
+    border_width = 1,
+    width = 200,
+    -- icon = "/usr/share/icons/gnome/24x24/actions/edia-playback-start.png", icon_size = 24,
+    run = function() play(); popup() end
+  })
+end
+---}}}
+
+
+---{{{ mocplay() 
+-- easier way to check|run mocp
+function play(plyrCmd) 
+
+  -- break on unknown commands
+  if not COMMANDS[plyrCmd] then return end
+
+  -- start server if not running
+  if trackinfo.state == "OFF" and not ( COMMANDS[plyrCmd] == COMMANDS["STOP"] ) then
+    awful.util.spawn('urxvt -e sh -c \"mocp --server\"',false)
+    state()
+
+    -- if starting, then turn most commands to play
+    if COMMANDS[plyrCmd] == COMMANDS["FWD"] or 
+        COMMANDS[plyrCmd] == COMMANDS["REV"] or
+        COMMANDS[plyrCmd] == COMMANDS["PAUSE"] then
+
+      plyrCmd = COMMANDS["PLAY"] 
+    end
+
+  end
+
+  awful.util.spawn(COMMANDS[plyrCmd],false)
+  state()
+  popup()
+
+end
+---}}}
+
+function setwidget(w)
+
+  settings.widget = w
+
+  -- assign buttons
+  settings.widget:buttons ({
+    button({}, 1, function() play("FWD")    end),
+    button({}, 2, function() play("PAUSE")  end),
+    button({}, 4, function() play("FWD")    end),
+    button({}, 3, function() play("REV")    end),
+    button({}, 5, function() play("REV")    end)
+  })
+
+  -- what to do when mouse over
+  settings.widget.mouse_enter = function() awful.hooks.timer.register(1,popup) end
+  settings.widget.mouse_leave = function() awful.hooks.timer.unregister(popup) end
+
+  awful.hooks.timer.register (settings.interval,scroller)
+  state()
+end
+
+---{{{ function update ( k, v)
+-- called by any kind of external script to trigger widget text update
+function update ( k, v )
+  if #k == 0 or #v == 0 then return end
+  if trackinfo[k] ~= nil then
+    trackinfo[k] = v
+  end
+  state()
+end
+---}}}
 
 -- vim: filetype=lua fdm=marker tabstop=2 shiftwidth=2 expandtab smarttab autoindent smartindent:
 
