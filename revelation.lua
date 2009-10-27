e67783af3b1ddfaf6faa23d0004738931a6e2960
perry <perry.hargrave@gmail.com>
2009-10-27 12:28:02 -0700

    fix to work with awesome 3.4-*
    No longer does anything with class filtering
    Use fair layout to expose clients

--- revelation.lua
+++ revelation.lua
@@ -1,13 +1,29 @@
----------------------------------------------------------------------------
+-- revelation.lua
+-- This is a modification of the original awesome library that implemented 
+-- expose like behavior. 
+-- 
+-- @author Perry Hargrave (aka bioe007)
+--          perry)dot(hargrave)at(gmail.com
+-- awesome v3.4-20-g8e02306 (Closing In)
+-- 
+-- original file information:
 -- @author Espen Wiborg &lt;espenhw@grumblesmurf.org&gt;
 -- @author Julien Danjou &lt;julien@danjou.info&gt;
 -- @copyright 2008 Espen Wiborg, Julien Danjou
--- @release v3.1-rc3-14-gc7ee83f
--- 
--- note: rewritten by Perry Hargrave perry)dot(hargrave)at(gmail.com
--- note: this is a version of revelation.lua modified for awesome 3.2-50
 --
----------------------------------------------------------------------------
+-- USE: 
+-- 1. save this file as $HOME/.config/awesome/revelation.lua
+-- 2. put near the top of your rc.lua
+--          require("revelation") 
+-- 3. make a global keybinding for revelation in your rc.lua:
+--          awful.key({ modkey }, "e",  revelation.revelation)
+-- 4. reload rc.lua and try the keybinding. It should bring all clients to the current tag and set the layout to fair.
+--    You can focus clients with cursor(arrow) or 'hjkl' keys then press <enter> to select or <escape> to abort
+--
+-- NOTES: I have dumbed this down to simply merge all clients to the current tag
+-- the class filter is of little use (to me?) but I could reimplement it 
+-- if anyone is interested
+-- 
 local math = math
 local table = table
 local pairs = pairs
@@ -25,6 +41,11 @@ local print = print
 --- Exposé implementation
 module("revelation")
 
+--{{{ clients
+-- a now unused filter to grab clients based on their class
+--
+-- @param class the class string to find
+-- @s the screen
 function clients(class, s)
   local clients
   if class then
@@ -39,21 +60,29 @@ function clients(class, s)
   end
   return clients
 end
+--}}}
 
+--{{{ selectfn
+-- executed when user selects a client from expose view
+--
+-- @param restore function to reset the current tags view
 function selectfn(restore)
   return function(c)
     restore()
     -- Pop to client tag
-    awful.tag.viewonly(c:tags()[1])
+    awful.tag.viewonly(c:tags()[1], c.screen)
     -- Focus and raise
     capi.client.focus = c
     c:raise()
   end
 end
+--}}}
 
---- Returns keyboardhandler.
--- Arrow keys move focus, Return selects, Escape cancels.
--- Ignores modifiers.
+--{{{ keyboardhandler
+-- Returns keyboardhandler.
+-- Arrow keys and 'hjkl' move focus, Return selects, Escape cancels. Ignores modifiers.
+--
+-- @param restore a function to call if the user presses escape 
 function keyboardhandler (restore)
   return function (mod, key, event)
     if event ~= "press" then return true end
@@ -73,7 +102,6 @@ function keyboardhandler (restore)
     --
     if key == "Escape" then
       restore()
-      -- awful.tag.history.restore()
       return false
     elseif key == "Return" then
       selectfn(restore)(capi.client.focus)
@@ -85,33 +113,23 @@ function keyboardhandler (restore)
     return true
   end
 end
+--}}}
 
---- Implement Exposé (from Mac OS X).
+--{{{ revelation
+-- Implement Exposé (from Mac OS X).
 -- @param class The class of clients to expose, or nil for all clients.
 -- @param fn A binary function f(t, n) to set the layout for tag t for n clients, or nil for the default layout.
 -- @param s The screen to consider clients of, or nil for "current screen".
 function revelation(class, fn, s)
   local scr = s or capi.mouse.screen
-  local t = awful.tag.selected()
+  local t = capi.screen[scr]:tags()[1] 
+  local oldlayout = awful.tag.getproperty( t, "layout" )
 
-  local oset = {}
-  oset.mwfact = awful.tag.getmwfact(t)
-  oset.ncol = awful.tag.getncol(t)
-  oset.nmaster = awful.tag.getnmaster(t)
-  oset.layout = awful.tag.getproperty(t,"layout")
-
-  awful.tag.setproperty(t,"layout",awful.layout.suit.fair )
-  awful.tag.viewmore( capi.screen[scr]:tags() )
-
-  awful.hooks.user.call("arrange", scr)
+  awful.tag.viewmore( capi.screen[scr]:tags(), t.screen )
+  awful.layout.set(awful.layout.suit.fair,t)
 
   local function restore()
-    local t = awful.tag.selected()
-  
-    awful.tag.setproperty(t,"layout",oset.layout)
-    awful.tag.setnmaster(oset.nmaster,t)
-    awful.tag.setmwfact(oset.mwfact, t)
-    awful.tag.setncol(oset.ncol, t)
+    awful.layout.set(oldlayout,t)
     awful.tag.viewonly(t)
 
     capi.keygrabber.stop()
@@ -119,3 +137,6 @@ function revelation(class, fn, s)
 
   capi.keygrabber.run(keyboardhandler(restore))
 end
+--}}}
+
+-- vim:set filetype=lua textwidth=120 fdm=marker tabstop=4 shiftwidth=4 expandtab smarttab autoindent smartindent: --
