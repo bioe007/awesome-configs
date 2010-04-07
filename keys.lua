ddf9c5575924b26d88783234c31400b805070404
perry <perry.hargrave@gmail.com>
2010-04-07 14:37:44 -0700

    keys.lua: use new key_gen() function to ? clean up keybindings
    key_gen(args, ...):
        creates a keybindings

--- keys.lua
+++ keys.lua
@@ -1,126 +1,216 @@
--- {{{ global keys
+
+-- some helper functins to make keybindings a bit more readable (?)
+function key_gen(args, ...)
+    --{{{ try to make keybindings look less messy
+    -- vargs[1] must be a function address suitable for awful.key()
+    local vargs = {...}
+    if args == nil or vargs == nil then return end
+
+    local key = args.key
+    nvargs = #vargs
+
+    -- sanity
+    if type(key) ~= 'string' then return end
+    if vargs ~= nil and type(vargs[1]) ~= 'function' then
+        print("key_gen: rec'vd non fx arg", vargs[1])
+        return
+    end
+
+    local modkeys = {}
+    if args.modifiers ~= nil and type(args.modifiers) ~= 'table' then
+        print("key_gen: rec'vd non table modifiers", args.modifiers)
+        return
+    else
+        modkeys = args.modifiers or {}
+    end
+
+    if args.use_modkey == nil or args.use_modkey == true then
+        table.insert(modkeys, settings.modkey)
+    end
+
+    local f = table.remove(vargs, 1)
+    if nvargs > 1 then
+        return awful.key.new(modkeys, key, function()
+            f(unpack(vargs)) end)
+    else
+        return awful.key(modkeys, key, f)
+    end
+
+end
+--}}}
+
+
+function key_app(args, ...)
+    --{{{ applications that are started by util.spawn...
+    -- i add "Mod1" to application starting
+    local vargs = {...}
+    local modkeys = args.modfiers or {}
+    table.insert(modkeys, "Mod1")
+    args.modifiers = modkeys
+
+    return key_gen(args, awful.util.spawn, unpack(vargs), false)
+
+end
+-- }}}
+
+
 globalkeys = awful.util.table.join(
-    awful.key({settings.modkey, "Control", "Shift" }, "t", function() print('called kb')
-        keygrabber.run(tag_strmatch())
-        end),                -- move to next tag
-    awful.key({settings.modkey}, "space", awful.tag.viewnext),                -- move to next tag
-    awful.key({settings.modkey, "Control"}, "space", workspace_next),          -- move to next tag on all screens
-    awful.key({settings.modkey, "Shift" }, "space", awful.tag.viewprev),       -- move to previous tag
-    awful.key({settings.modkey, "Control", "Shift"}, "space", workspace_prev), -- move to previous tag on all screens
-    awful.key({settings.modkey}, "j", function() awful.client.focus.byidx( 1) end),
-    awful.key({settings.modkey}, "k", function() awful.client.focus.byidx(-1) end),
-    awful.key({settings.modkey}, "e",  revelation.revelation),
+    --{{{ global keys
+    key_gen({key = "t", modifiers = {"Control", "Shift"}},
+        keygrabber.run, tag_strmatch),
+    key_gen({modifiers = {"Control"}, key = "r"}, awesome.restart),
+    key_gen({modifiers = {"Shift"}, key = "q"}, awesome.quit),
+    key_gen({key = "space"}, awful.tag.viewnext),
+    key_gen({key = "space", modifiers = {"Shift"}}, awful.tag.viewprev),
+    key_gen({key = "space", modifiers = {"Control"}}, workspace_next),
+    key_gen({key = "space", modifiers = {"Control", "Shift"}}, workspace_prev),
+    key_gen({key = "j"}, awful.client.focus.byidx, 1),
+    key_gen({key = "k"}, awful.client.focus.byidx, -1),
+    key_gen({key = "e"}, revelation.revelation),
 
-    -- {{{ shiftycentric
-    awful.key({ settings.modkey            }, "Escape", awful.tag.history.restore), -- move to prev tag by history
-    awful.key({ settings.modkey, "Shift"   }, "n",      shifty.send_prev),          -- move client to prev tag
-    awful.key({ settings.modkey            }, "n",      shifty.send_next),          -- move client to next tag
-    awful.key({ settings.modkey, "Control" }, "n",      tag_to_screen ),            -- move a tag to next screen
-    awful.key({ settings.modkey, "Shift"   }, "r",      shifty.rename),             -- rename a tag
-    awful.key({ settings.modkey            }, "d",      shifty.del),                -- delete a tag
-    awful.key({ settings.modkey, "Shift"   }, "a",      shifty.add),                -- creat a new tag
-    -- }}}
+    --{{{ shiftycentric
+    key_gen({key = "Escape"}, awful.tag.history.restore),
+    key_gen({key = "n"}, shifty.send_next),
+    key_gen({key = "n", modifiers = {"Shift"}}, shifty.send_prev),
+    key_gen({key = "n", modifiers = {"Control"}}, tag_to_screen),
+    key_gen({key = "r", modifiers = {"Shift"}}, shifty.rename),
+    key_gen({key = "d", modifiers = {"Shift"}}, shifty.del),
+    key_gen({key = "a", modifiers = {"Shift"}}, shifty.add),
+    --}}}
 
-    -- {{{ Layout manipulation
-    awful.key({ settings.modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1) end),
-    awful.key({ settings.modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1) end),
-    awful.key({ settings.modkey }, "s", function () awful.screen.focus_relative(1) end),
-    awful.key({ settings.modkey, "Shift" }, "s", awful.client.movetoscreen),   -- switch client to other screen
-    awful.key({ settings.modkey,           }, "u", awful.client.urgent.jumpto),
-    awful.key({ settings.modkey,           }, "Tab", function ()
-            awful.client.focus.history.previous()
-            if client.focus then client.focus:raise() end
-            end),
-    -- }}}
+    --{{{ Layout manipulation
+    key_gen({key = "j", modifiers = {"Shift"}}, awful.client.swap.byidx, 1),
+    key_gen({key = "k", modifiers = {"Shift"}}, awful.client.swap.byidx, -1),
+    key_gen({key = "s"}, awful.screen.focus_relative, 1),
+    key_gen({key = "s", modifiers = {"Shift"}}, awful.client.movetoscreen),
+    key_gen({key = "u"}, awful.client.urgent.jumpto),
+    key_gen({key = "Tab"}, function()
+        awful.client.focus.history.previous()
+        if client.focus then client.focus:raise() end
+    end),
+    --}}}
 
-    -- {{{ - APPLICATIONS
-    awful.key({ settings.modkey }, "Return", function () awful.util.spawn(settings.apps.terminal,false) end),
+    --{{{ APPLICATIONS
+    key_gen({key = "Return"}, function()
+        awful.util.spawn(settings.apps.terminal, false) end),
+    key_app({key="f"}, settings.apps.filemgr),
+    key_app({key="c"}, "galculator"),
+    key_app({modifiers = {"Shift"}, key = "g"}, "gimp"),
+    key_app({key = "v"}, "/home/perry/.bin/vim-start.sh"),
 
     -- run or raise type behavior but with benefits of shifty
-    awful.key({ settings.modkey},"w", function () if not tagSearch("web") then awful.util.spawn(settings.apps.browser) end end),
-    awful.key({ settings.modkey },"m", function () if not tagSearch("mail") then awful.util.spawn(settings.apps.mail) end end),
-    awful.key({ settings.modkey, "Mod1", "Shift" },"v", function () if not tagSearch("vbx") then awful.util.spawn('VBoxSDL -vm xp2') end end),
-    awful.key({ settings.modkey },"g", function () if not tagSearch("dz") then awful.util.spawn('gschem') end end),
-    awful.key({ settings.modkey },"p", function () if not tagSearch("dz") then awful.util.spawn('pcb') end end),
+    key_gen({key = "w"}, function()
+        if not tagSearch("web") then
+            awful.util.spawn(settings.apps.browser)
+        end
+    end),
+    key_gen({key = "m"}, function()
+        if not tagSearch("mail") then
+            awful.util.spawn(settings.apps.mail)
+        end
+    end),
+    key_gen({modifiers = {"Mod1", "Shift"}, key = "v"}, function()
+        if not tagSearch("vbx") then
+            awful.util.spawn('VBoxSDL -vm xp2')
+        end
+    end),
+    key_gen({key = "g"}, function()
+        if not tagSearch("dz") then
+            awful.util.spawn('gschem')
+        end
+    end),
+    key_gen({key = "p"}, function()
+        if not tagSearch("dz") then
+            awful.util.spawn('pcb')
+        end
+    end),
+    --}}}
 
-    awful.key({ settings.modkey, "Mod1" },"f", function () awful.util.spawn(settings.apps.filemgr) end),
-    awful.key({ settings.modkey, "Mod1" },"c", function () awful.util.spawn("galculator",false) end),
-    awful.key({ settings.modkey, "Mod1", "Shift" } ,"g", function () awful.util.spawn('gimp') end),
-    awful.key({ settings.modkey, "Mod1" },"o", function () awful.util.spawn('/home/perry/.bin/octave-start.sh',false) end),
-    awful.key({ settings.modkey, "Mod1" },"v", function () awful.util.spawn('/home/perry/.bin/vim-start.sh',false) end),
-    awful.key({ settings.modkey, "Mod1" },"i", function () awful.util.spawn('gtkpod',false) end),
-    -- }}}
-
-    -- {{{ - MEDIA
+    -- {{{- MEDIA
     -- music player
-    awful.key({ settings.modkey, "Mod1" }, "p", function() mocp.play("PLAY") end),
-    awful.key({},"XF86AudioPlay",               function() mocp.play("PLAY") end),
-    awful.key({ settings.modkey },"Down",       function() mocp.play("FWD") end ),
-    awful.key({ settings.modkey },"Up",         function() mocp.play("REV") end),
-    awful.key({},"XF86AudioPrev",               function() mocp.play("REV") end),
-    awful.key({},"XF86AudioNext",               function() mocp.play("FWD") end ),
-    awful.key({},"XF86AudioStop",               function() mocp.play("STOP") end),
-    awful.key({},"XF86AudioRaiseVolume", function() volume.vol("up","5") end),
-    awful.key({},"XF86AudioLowerVolume", function() volume.vol("down","5") end),
-    awful.key({},"XF86AudioMute", function() volume.vol() end),
-    -- }}} 
-    
-    -- {{{ WM
-    awful.key({ settings.modkey, "Control" }, "r", awesome.restart),
-    awful.key({ settings.modkey, "Shift"   }, "q", awesome.quit),
+    key_gen({key = "p"}, mocp.play, "PLAY"),
+    key_gen({key = "Down"}, mocp.play, "FWD"),
+    key_gen({key = "Up"}, mocp.play, "REV"),
+    key_gen({key = "XF86AudioPlay", use_modkey = false}, mocp.play, "PLAY"),
+    key_gen({key = "XF86AudioPrev", use_modkey = false}, mocp.play, "REV"),
+    key_gen({key = "XF86AudioNext", use_modkey = false}, mocp.play, "FWD"),
+    key_gen({key = "XF86AudioStop", use_modkey = false}, mocp.play, "STOP"),
+    key_gen({key = "XF86AudioRaiseVolume", use_modkey = false}, volume.vol,
+        "up", "5"),
+    key_gen({key = "XF86AudioLowerVolume", use_modkey = false}, volume.vol,
+        "down", "5"),
+    key_gen({key = "XF86AudioMute", use_modkey = false}, volume.vol ),
+    --}}}
 
-    awful.key({ settings.modkey,           }, "l",     function () awful.tag.incmwfact( 0.03)    end),
-    awful.key({ settings.modkey,           }, "h",     function () awful.tag.incmwfact(-0.03)    end),
-    awful.key({ settings.modkey,           }, "q",     function (c) awful.client.incwfact( 0.03,c)    end),
-    awful.key({ settings.modkey,           }, "a",     function (c) awful.client.incwfact( -0.03,c)    end),
-    awful.key({ settings.modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ settings.modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ settings.modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ settings.modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    awful.key({ settings.modkey, "Mod1"    }, "l", function () awful.layout.inc(settings.layouts,  1) end),
-    awful.key({ settings.modkey, "Mod1", "Shift"   }, "l", function () awful.layout.inc(settings.layouts, -1) end),
+    --{{{ Clients
+    key_gen({key = "q"}, awful.client.incwfact, 0.03),
+    key_gen({key = "a"}, awful.client.incwfact, -0.03),
+    key_gen({key = "l"}, awful.tag.incmwfact, 0.03),
+    key_gen({key = "h"}, awful.tag.incmwfact, -0.03),
+    key_gen({key = "h", modifiers = {"Shift"}}, awful.tag.incnmaster, 1),
+    key_gen({key = "l", modifiers = {"Shift"}}, awful.tag.incnmaster, -1),
+    key_gen({key = "h", modifiers = {"Control"}}, awful.tag.incncol, 1),
+    key_gen({key = "l", modifiers = {"Control"}}, awful.tag.incncol, -1),
+    key_gen({key ="l", modifiers = {"Mod1"}}, awful.layout.inc,
+            settings.layouts, 1),
+    key_gen({key = "l", modifiers = {"Mod1", "Shift"}}, awful.layout.inc,
+            settings.layouts, -1),
 
     -- Prompt
-    awful.key({ settings.modkey },            "F1",     function () widgets.promptbox[mouse.screen]:run() end),
-    -- }}}
+    key_gen({key = "F1"}, function() widgets.promptbox[mouse.screen]:run() end),
+    key_gen({key = "F2"}, function()
+        widgets.promptbox[mouse.screen]:run(nil, nil, function(args)
+            cmd = "urxvt -name man -e zsh -c \'man "
+            cmd = cmd .. unpack(args) .. "\'"
+            print("MANKB::::::::::::::: ",cmd)
+            awfult.util.spawn_with_shell(cmd)
+        end) end),
+    --}}}
 
-    -- {{{ - POWER
-    awful.key({ settings.modkey, "Mod1" },"h", function () awful.util.spawn('sudo pm-hibernate',false) end),
-    awful.key({ settings.modkey, "Mod1" },"s", function () 
+    -- {{{- POWER
+    key_app({key = "h"}, 'sudo pm-hibernate'),
+    key_app({key = "r"}, 'sudo reboot'),
+    key_gen({key = "s", modifiers = {"Mod1"}}, function()
         awful.util.spawn('slock',false)
         os.execute('sudo pm-suspend')
     end),
-    awful.key({ settings.modkey, "Mod1" },"r", function () awful.util.spawn('sudo reboot',false) end),
-    -- }}} 
+    --}}}
 
-    awful.key({ settings.modkey }, "F4", function() awful.util.spawn('/home/perry/.bin/stupid.sh --soyo') end),
-    awful.key({ settings.modkey }, "F5", function() awful.util.spawn('/home/perry/.bin/stupid.sh --sync --pos left-of') end),
-    awful.key({ settings.modkey }, "F6", function() awful.util.spawn('/home/perry/.bin/stupid.sh --off') end)
-    )
-    -- }}}
+    -- monitors
+    key_app({key = "F4"}, '/home/perry/.bin/stupid.sh --soyo'),
+    key_app({key = "F5"}, '/home/perry/.bin/stupid.sh --sync --pos left-of'),
+    key_app({key = "F6"}, '/home/perry/.bin/stupid.sh --off')
+)
+--}}}
 
--- Compute the maximum number of digit we need, limited to 9
 for i = 1, 9 do
+    --{{{ bind the numeric keys to 'normal' awesome keybindings
     globalkeys = awful.util.table.join(globalkeys,
-    awful.key({ settings.modkey }, i, function()
-        awful.tag.viewonly(shifty.getpos(i)) end),
-    awful.key({ settings.modkey, "Control" }, i, function()
-        this_screen = awful.tag.selected().screen
-        t = shifty.getpos(i, this_screen) 
-        t.selected = not t.selected
-    end),
-    awful.key({ settings.modkey, "Shift" }, i, function ()
-        if client.focus then 
-            local c = client.focus
-            slave = not ( client.focus == awful.client.getmaster(mouse.screen))
-            t = shifty.getpos(i)
-            awful.client.movetotag(t,c)
-            awful.tag.viewonly(t)
-            if slave then awful.client.setslave(c) end
-        end 
-    end)
+        awful.key({settings.modkey}, i, function()
+            awful.tag.viewonly(shifty.getpos(i))
+        end),
+
+        awful.key({settings.modkey, "Control"}, i, function()
+            this_screen = awful.tag.selected().screen
+            t = shifty.getpos(i, this_screen)
+            t.selected = not t.selected
+        end),
+
+        awful.key({settings.modkey, "Shift"}, i, function()
+            if client.focus then
+                local c = client.focus
+                slave = not (client.focus ==
+                                awful.client.getmaster(mouse.screen))
+                t = shifty.getpos(i)
+                awful.client.movetotag(t,c)
+                awful.tag.viewonly(t)
+                if slave then awful.client.setslave(c) end
+            end
+        end)
     )
 end
+--}}}
 
 return globalkeys
--- vim:set filetype=lua textwidth=120 fdm=marker tabstop=4 shiftwidth=4 expandtab smarttab autoindent smartindent: --
+
+-- vim:set ft=lua fdm=marker ts=4 sw=4 et ai si: --
