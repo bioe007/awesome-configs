81f4ceaf71a5b3f54ef9a7daa5904a82aecd681a
Perry Hargrave <resixian@gmail.com>
2011-04-25 14:38:40 -0700

    Some clean up and updating.

--- awesome.lua
+++ awesome.lua
@@ -11,10 +11,7 @@ require("naughty")
 -- custom modules
 require("markup")
 require("shifty")
-require("mocp")
 require("calendar")
-require("battery")
-require("markup")
 require("fs")
 require("volume")
 require("vicious")
@@ -22,28 +19,25 @@ require("revelation")
 print("Modules loaded: " .. os.time())
 
 function tag_move(t, scr)
-    -- {{{
     local ts = t or awful.tag.selected()
     local screen_target = scr or awful.util.cycle(screen.count(), ts.screen + 1)
 
-    shifty.set(ts, { screen = screen_target })
+    shifty.set(ts, {screen = screen_target})
 end
--- }}}
 
 function tag_to_screen(t, scr)
-    -- {{{
     local ts = t or awful.tag.selected()
     local screen_origin = ts.screen
     local screen_target = scr or awful.util.cycle(screen.count(), ts.screen + 1)
 
-    awful.tag.history.restore(ts.screen,1)
+    awful.tag.history.restore(ts.screen, 1)
     tag_move(ts, screen_target)
 
     -- never waste a screen
     if #(screen[screen_origin]:tags()) == 0 then
         for _, tag in pairs(screen[screen_target]:tags()) do
             if not tag.selected then
-                tag_move(tag, screen_origin) 
+                tag_move(tag, screen_origin)
                 tag.selected = true
                 break
             end
@@ -56,26 +50,23 @@ function tag_to_screen(t, scr)
         local c = ts:clients()[1]
         client.focus = c
     end
-
 end
--- }}}
 
 function workspace_next()
-    for s=1,screen.count() do
+    for s = 1, screen.count() do
         awful.tag.viewnext(screen[s])
     end
 end
 
 function workspace_prev()
-    for s=1,screen.count() do
+    for s = 1, screen.count() do
         awful.tag.viewprev(screen[s])
     end
 end
 
 function tagSearch(name)
-    -- {{{
   for s = 1, screen.count() do
-    t = shifty.name2tag(name,s)
+    t = shifty.name2tag(name, s)
     if t ~= nil then
         if t.screen ~= mouse.screen then
             awful.screen.focus(t.screen)
@@ -86,32 +77,33 @@ function tagSearch(name)
   end
   return false
 end
--- }}}
 
--- {{{ wip
+function player(command)
+    awful.util.spawn('/usr/bin/sonata ' .. command)
+end
+
 function tagScreenless()
     local allTags = {}
     local curTag = awful.tag.selected()
     for s = 1, screen.count() do
-        t = shifty.name2tag(name,s)
+        t = shifty.name2tag(name, s)
         if t ~= nil then
             awful.tag.viewonly(t)
-            awful.screen.focus(awful.util.cycle(screen.count(),s+mouse.screen))
+            awful.screen.focus(awful.util.cycle(screen.count(),
+                                                s + mouse.screen))
             return true
         end
     end
     return false
 end
--- }}}
 
--- Called externally and just pops to or merges with my active vim server when 
--- new files are dumped to it. (vim-start.sh) 
+-- Called externally and just pops to or merges with my active vim server when
+-- new files are dumped to it. (vim_start)
 -- though it could easily be used with any tag by passing a different 'name'
 -- parameter
 function tagPop(name)
-    -- {{{ tagPop() 
     for s = 1, screen.count() do
-        t = shifty.name2tag(name,s)
+        t = shifty.name2tag(name, s)
         if t ~= nil then
             if t.screen == awful.tag.selected().screen then
                 t.selected = true
@@ -122,69 +114,62 @@ function tagPop(name)
         end
     end
 end
--- }}}
 
-settings   = dofile(awful.util.getdir("config") .."/settings.lua")
-widgets    = dofile(awful.util.getdir("config") .. "/widgets.lua")
-globalkeys = dofile(awful.util.getdir("config") .. "/keys.lua")
+cdir = awful.util.getdir("config")
+passwords  = dofile(cdir .."/PASSWORDS.lua")
+settings   = dofile(cdir .."/settings.lua")
+widgets    = dofile(cdir .. "/widgets.lua")
+globalkeys = dofile(cdir .. "/keys.lua")
 
--- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    awful.button({}, 3, function() mymainmenu:toggle() end),
+    awful.button({}, 4, awful.tag.viewnext),
+    awful.button({}, 5, awful.tag.viewprev)
 ))
--- }}}
 
---{{{ clientkeys
 clientkeys = awful.util.table.join(
-    awful.key({ settings.modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
-    awful.key({ settings.modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
-    awful.key({ settings.modkey, "Shift"   }, "0",      function (c) c.sticky=not c.sticky            end),
-    awful.key({ settings.modkey, "Mod1" }, "space",  awful.client.floating.toggle                     ),
-    awful.key({ settings.modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
-    awful.key({ settings.modkey,           }, "o",      awful.client.movetoscreen                        ),
-    awful.key({ settings.modkey, "Mod1"    }, "n",      function (c) c.minimized = not c.minimized    end),
-    awful.key({ settings.modkey, "Control"}, "m",
-        function (c)
+    awful.key({settings.modkey,}, "f",
+        function(c) c.fullscreen = not c.fullscreen  end),
+    awful.key({settings.modkey, "Shift"}, "c", function(c) c:kill() end),
+    awful.key({settings.modkey, "Shift"}, "0",
+        function(c) c.sticky = not c.sticky end),
+    awful.key({settings.modkey, "Mod1"}, "space", awful.client.floating.toggle),
+    awful.key({settings.modkey, "Control"}, "Return",
+        function(c) c:swap(awful.client.getmaster()) end),
+    awful.key({settings.modkey,}, "o", awful.client.movetoscreen),
+    awful.key({settings.modkey, "Mod1"}, "n",
+        function(c) c.minimized = not c.minimized end),
+    awful.key({settings.modkey, "Control"}, "m",
+        function(c)
             c.maximized_horizontal = not c.maximized_horizontal
             c.maximized_vertical   = not c.maximized_vertical
             c:raise()
         end)
     )
--- }}}
 
--- Set keys
 root.keys(globalkeys)
 
 shifty.config.clientkeys = clientkeys
 shifty.taglist = widgets.taglist
 shifty.init()
 
--- {{{ signals
-client.add_signal("focus", function (c)
-
+client.add_signal("focus", function(c)
     c.border_color = beautiful.border_focus
 
-    if settings.opacity[c.class] then 
+    if settings.opacity[c.class] then
        c.opacity = settings.opacity[c.class].focus
     else
         c.opacity = settings.opacity.default.focus or 1
     end
     c:raise()
-end) 
+end)
 
--- Hook function to execute when unfocusing a client.
-client.add_signal("unfocus", function (c)
-
+client.add_signal("unfocus", function(c)
     c.border_color = beautiful.border_normal
 
-    if settings.opacity[c.class] then 
+    if settings.opacity[c.class] then
         c.opacity = settings.opacity[c.class].unfocus
     else
         c.opacity = settings.opacity.default.unfocus or 0.7
     end
 end)
--- }}}
-
--- vim:set filetype=lua textwidth=120 fdm=marker tabstop=4 shiftwidth=4 expandtab smarttab autoindent smartindent: --
