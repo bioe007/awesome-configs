8e2e802f643f4caee70de3bcf324d4c78c8a5afb
Perry Hargrave <bioe007@users.noreply.github.com>
2024-02-25 11:59:00 -0800

    basically disabled in favor of .xprofile

--- autorun.sh
+++ autorun.sh
@@ -7,19 +7,20 @@ function run {
     fi
 }
 
-xrandr --output DP-4 --mode 3440x1440 --rate 75.05
-xrandr --output DP-0 --rotate left --left-of DP-4 --output DP-4 --primary
-nvidia-settings --assign CurrentMetamode="DP-4: 3440x1440_75 +1440+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}, DP-0: 2560x1440 +0+0 {rotation=left, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+# xrandr --output DP-4 --mode 3440x1440 --rate 75.05
+# xrandr --output DP-0 --rotate left --left-of DP-4 --output DP-4 --primary
+# nvidia-settings --assign CurrentMetamode="DP-4: 3440x1440_75 +1440+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}, DP-0: 2560x1440 +0+0 {rotation=left, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
 
 
-sleep 1
-run "/usr/bin/nitrogen --restore"
-run "/usr/bin/playerctld"
-run "/usr/bin/blueman-applet"
-run "/usr/bin/slack -u"
-run "/usr/bin/brave"
-run "/usr/bin/youtube-music"
+# sleep 1
+# run "/usr/bin/nitrogen --restore"
+# run "/usr/bin/playerctld"
+# run "/usr/bin/blueman-applet"
+# run "/usr/bin/slack -u"
+# run "/usr/bin/brave"
+# run "/usr/bin/youtube-music"
+# run "bitwarden-desktop"
 
-# kdeconnect-indicator is safe to restart, the export is to fix its colorscheme
-export QT_QPA_PLATFORMTHEME=gtk2 && kdeconnect-indicator
+# # kdeconnect-indicator is safe to restart, the export is to fix its colorscheme
+# export QT_QPA_PLATFORMTHEME=gtk2 && kdeconnect-indicator
 #run "/usr/bin/redshift -t 6500:5800"
